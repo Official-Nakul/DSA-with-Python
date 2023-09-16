f4280f14cff37551979bb3b1356bfc32a82a3d80
Official-Nakul <visavadianakulyt@gmail.com>
2023-09-17 01:28:21 +0530

    Stack with Python

--- Stack.py
+++ Stack.py
@@ -26,14 +26,30 @@ class Stack:
     def change(self, inx, val):
         if self.head is None:
             raise Exception("Stack is Empty")
+        elif inx < 0 or inx > self.get_length():
+            raise Exception("Invalid index")
         ptr = self.head
         count = 0
         while ptr:
             if count == inx:
                 ptr.data = val
+                print(f"Value at Index {inx} is updated with {val}")
                 break
             count += 1
             ptr = ptr.next
+
+    # ------------ Update Stack with Value ----------------
+    def replace(self, new_val, old_val):
+        if self.head is None:
+            raise Exception("Stack is Empty")
+        ptr = self.head
+        while ptr:
+            if ptr.data == old_val:
+                ptr.data = new_val
+                print(f"{old_val} is updated with {new_val}")
+                return
+            ptr = ptr.next
+        print(f"{old_val} is not in the Stack")
 
     # ------------ Prints the Stack----------------
     def show(self):
@@ -58,14 +74,16 @@ class Stack:
 
 s = Stack()
 
-user_opt = (1, 2, 3, 4, 5, 6)
+user_opt = (1, 2, 3, 4, 5, 6, 7, 8)
 
 print("1.Push\n\
 2.Pop\n\
 3.Peek\n\
 4.Print the Stack\n\
 5.Get Length of the Stack\n\
-6.Exit")
+6.Update value with index\n\
+7.Replace value\n\
+8.Exit")
 print("-------------------------------")
 
 # ---------------------------- Loop to ask user choice multiple times --------------------------
@@ -88,12 +106,17 @@ while True:
         elif user_input == 5:
             print(f"Length of Stack is {s.get_length()}")
             print("***********************")
-        elif user_input == 5:
+        elif user_input == 6:
             inx = int(input("Enter Index: "))
             val = input("Enter value: ")
-            print(f"Length of Stack is {s.change(inx, val)}")
+            s.change(inx, val)
             print("***********************")
         elif user_input == 7:
+            old_val = input("Enter Old Value: ")
+            new_val = input("Enter New Value: ")
+            s.replace(new_val, old_val)
+            print("***********************")
+        elif user_input == 8:
             break
     else:
         print("Enter Valid Number")
