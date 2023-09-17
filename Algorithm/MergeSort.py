a73a29b207c7967ab5bf682cf6c27d9cf213920b
Official-Nakul <visavadianakulyt@gmail.com>
2023-09-17 13:45:01 +0530

    Merge Sort

--- Algorithm/MergeSort.py
+++ Algorithm/MergeSort.py
@@ -28,6 +28,6 @@ def merge(num, left, mid, right):
         num[left+i] = temp[i]
 
 
-num = [4, 6, 0, 1, 2, 3, 7, 8, 9, 5, -1, -3, -4]
+num = [4, 6, 0, 1, 2, 3, 7, 8, 9, 5, -1, -3, -4, -2]
 divide(num, 0, len(num)-1)
 print(num)
