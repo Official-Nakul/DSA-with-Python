c84be191da8ccbda2b5dc52292e92dfa6f059970
Official-Nakul <visavadianakulyt@gmail.com>
2023-09-17 17:14:48 +0530

    Optimized Version of Binary Search

--- Algorithm/binary_search.py
+++ Algorithm/binary_search.py
@@ -1,27 +1,23 @@
 def binary_search(num, i, j, target):
-
     left = i
     right = j
     while left <= right:
-        mid = left+(right-left)//2
+        mid = left + (right - left) // 2
         if target == num[mid]:
             return mid
         elif target < num[mid]:
-            right = mid-1
+            right = mid - 1
         else:
-            left = mid+1
-    return mid
+            left = mid + 1
+    return left - 1
 
 
 num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-target = int(input("Enter Element you wnat to Find: "))
+target = int(input("Enter Element you want to Find: "))
 
-inx = binary_search(num, 0, len(num)-1, target)
+inx = binary_search(num, 0, len(num) - 1, target)
 
 if target == num[inx]:
     print(f"Element Found at Index {inx}")
 else:
-    if num[0] > target:
-        print(f"Element should be Inserted at Index 0")
-    else:
-        print(f"Element should be Inserted at Index {inx+1}")
+    print(f"Element should be Inserted at Index {inx + 1}")
